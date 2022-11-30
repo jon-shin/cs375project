eed985f55a60bb57070a45fadc5756537b010843
Will Bezenah <wbezenah29@gmail.com>
2022-11-30 14:06:32 -0500

    modified GraphGenerator to ensure connectivity

--- cs375Project/GraphGenerator.java
+++ cs375Project/GraphGenerator.java
@@ -22,13 +22,26 @@ public class GraphGenerator {
 		FileWriter fOutput = new FileWriter(fileName);
 		
 		int numV = random.nextInt(maxV - minV) + minV;
-		double density = sparse ? random.nextDouble(0.5) : (random.nextDouble(0.25) + 0.75);
+		double density = sparse ? random.nextDouble() * 0.5 : (random.nextDouble() * 0.25 + 0.75);
 		int numE = (int)(density / 2.0 * numV * (numV - 1));
 		
 		fOutput.write(numV + "\n");
 		
 		Set<String> edges = new HashSet<String>();
 		int count = 0;
+		for(int i = 0; i < numV; i++) {		//this new for loop ensures connectivity
+			int v2 = random.nextInt(numV);
+			while(i == v2) {v2 = random.nextInt(numV);}
+			String e = i + " " + v2;
+			String e2 = v2 + " " + i;
+			if(!edges.contains(e) && !edges.contains(e2)) {
+				edges.add(e);
+				edges.add(e2);
+				count++;
+				int weight = random.nextInt(MAX_WEIGHT-1) + 1;
+				fOutput.write(i + " " + v2 + " " + weight + "\n");
+			}
+		}
 		while(count < numE) {
 			int v1 = random.nextInt(numV);
 			int v2 = random.nextInt(numV);
@@ -54,8 +67,10 @@ public class GraphGenerator {
 	public static void main(String[] args) throws IOException{
 		GraphGenerator g = new GraphGenerator();
 		
-		g.generateGraph("gOutput.txt", 5, 10, false);
-		g.generateGraph("gOutputSparse.txt", 5, 10, true);
-		g.generateGraph("gOutputBig.txt", 20, 40, true);
+		//g.generateGraph("gOutput.txt", 5, 10, false);
+		//g.generateGraph("gOutputSparse.txt", 5, 10, true);
+		//g.generateGraph("gOutputBig.txt", 20, 40, true);
+		
+		g.generateGraph("gOutputSmallSparse.txt", 4, 6, true);
 	}
 }
