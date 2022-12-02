b9dc8abedd1796c0eb202c582160b4c945efead6
jon-shin <jshin75@binghamton.edu>
2022-12-02 17:05:26 -0500

    Added comments for adjList and edge classes

--- cs375Project/Edge.java
+++ cs375Project/Edge.java
@@ -1,46 +1,44 @@
+//This class creates an edge object, which is used by the class adjList.java
 package cs375Project;
 import java.util.*;
 public class Edge{
-    private int weight;
-    private boolean included = false;
-    private int source;
-    private int dest;
+    private int weight;     //Edge's weight
+    private int source;     //The vertex which the edge is coming from
+    private int dest;       //The vertex which the edge is going to
 
+    //constructor for edge
     public Edge(int source, int dest, int weight){
         this.source = source;
         this.dest = dest;
         this.weight = weight;
-        this.included = false;
     }
 
+    //getter for source
     public int getSource() {
         return source;
     }
 
+    //setter for source
     public void setSource(int source) {
         this.source = source;
     }
 
+    //getter for weight
     public int getWeight() {
         return weight;
     }
 
+    //setter for weight
     public void setWeight(int weight) {
         this.weight = weight;
     }
 
-    public boolean isIncluded() {
-        return included;
-    }
-
-    public void setIncluded(boolean included) {
-        this.included = included;
-    }
-
+    //getter for destination
     public int getDest() {
         return dest;
     }
 
+    //setter for destination
     public void setDest(int dest) {
         this.dest = dest;
     }
--- cs375Project/adjList.java
+++ cs375Project/adjList.java
@@ -1,18 +1,21 @@
+//This is the implementation of an adjacency list using Prim's algorithm
 package cs375Project;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
 public class adjList{
-    private static Comparator<Edge> minWeight;
-    private ArrayList<ArrayList<Edge>> graph;
-    private int numV;
+    private static Comparator<Edge> minWeight;  //Used to sort priority queue by minimum weight first
+    private ArrayList<ArrayList<Edge>> graph;   //Sets the adjacency list using a double ArrayList of Edges
+    private int numV;   //Number of vertices in the graph
 
+    //constructor for adjacency list
     public adjList(ArrayList<ArrayList<Edge>> graph, int numV){
         this.graph = graph;
         this.numV = numV;
     }
 
+    //Sets the comparator to order priority queue of edges by minimum weight
     static{
         minWeight = new Comparator<Edge>(){
             @Override
@@ -22,49 +25,54 @@ public class adjList{
         };
     }
 
+    //Creates edge objects from the input file and puts them into the adjacency list
     public void addEdge(int source, int destination, int weight){
         Edge e1 = new Edge(source, destination, weight);
         Edge e2 = new Edge(destination, source, weight);
-        graph.get(source).add(e1);
+        graph.get(source).add(e1);  
         graph.get(destination).add(e2);
     }
 
+    //Prim's algorithm with adjacency list
+    //Writes to a given output file
     public void primsMST(FileWriter w) throws IOException{
-        double startTime = System.nanoTime();
-        double endTime = 0;
+        double startTime = System.nanoTime();   //Starts the timer
+        double endTime = 0;     //Signifies end of timer
         w.write("Prim's Algorithm using adjacency list representation:\n");
 		w.write("Edge : Weight\n");
-        PriorityQueue<Edge> pq = new PriorityQueue<Edge>(minWeight);
-        ArrayList<Edge> mst = new ArrayList<Edge>();
+        PriorityQueue<Edge> pq = new PriorityQueue<Edge>(minWeight);    //Create the min priority queue for edges
+        ArrayList<Edge> mst = new ArrayList<Edge>();        //Creates the minimum spanning tree
         boolean selected[] = new boolean[numV];	//keep track of selected vertices
 		selected[0] = true; 					//arbitrarily select first vertex
-        int currentNode = 0;
+        int currentNode = 0;                    //Signifies which node is being added to the mst
         for(Edge e : graph.get(currentNode)){
-            pq.add(e);
+            pq.add(e);  //Add edges adjacent to starting node to the min priority queue
         }
         while(!pq.isEmpty()){
-            Edge minEdge = pq.peek();
-            currentNode = minEdge.getDest();
-            pq.remove();
+            Edge minEdge = pq.peek();           //Takes the edge at the top of the queue
+            currentNode = minEdge.getDest();    //Set the destination of the edge as the next node to be added
+            pq.remove();                        //Remove the minimum edge from the priority queue
+            //if the node is not already in the min spanning tree
             if (!selected[currentNode]) {
-                selected[currentNode] = true;
-                mst.add(minEdge);
+                selected[currentNode] = true;   //Signify that the node is added
+                mst.add(minEdge);               //add to minimum spanning tree
                 // Iterate through all the nodes adjacent to the node taken out of priority queue.
-                // Push only those nodes that are not yet present in the minumum spanning tree.
                 for (Edge e : graph.get(currentNode)) {
                     int adj_node = e.getDest();
                     if (selected[adj_node] == false) {
+                        // Push only those nodes that are not yet present in the minumum spanning tree.
                         pq.add(e);
                     }
                 }
             }
         }
+        //Print results of min spanning tree to output file
         for(int i = 0; i < mst.size(); i++){
             w.write(mst.get(i).getSource() + " - " + mst.get(i).getDest() + " : " + mst.get(i).getWeight() + "\n");
         }
         w.write("Total Computation Time: ");
-        endTime = System.nanoTime();
-        w.write(Double.toString((endTime-startTime)/1000000) + " milliseconds");
+        endTime = System.nanoTime();        //Stop timer
+        w.write(Double.toString((endTime-startTime)/1000000) + " milliseconds");    //Get the time in milliseconds
     }
 
     public static void main(String args[]) throws IOException{
